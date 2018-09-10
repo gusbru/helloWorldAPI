641d55f590816fa33af0006d55cc4b3245e72308
Gustavo Brunetto <gustavobruneto@gmail.com>
2018-09-10 18:51:11 -0300

    func: use the decoder to receive the payload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 const http = require('http');
 const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 // create the server
 const server = http.createServer((req, res) => {
@@ -15,7 +16,24 @@ const server = http.createServer((req, res) => {
   const path = parsedUrl.pathname;
   const trimmedPath = path.replace(/^\/+|\/+$/g, '');
 
+  // http method
+  const method = req.method.toLowerCase();
+
+  // payload
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on('end', () => {
+    buffer += decoder.end();
+  });
+
+
   console.log(trimmedPath);
+  console.log(method);
 
   res.end("From server\n");
 });
