5276965db66426b4a87a243142a108e9fb1a1c7e
Gustavo Brunetto <gustavobruneto@gmail.com>
2018-09-11 12:41:50 -0300

    refactor: create directories and put the unified server on a separated function

--- index.js
+++ index.js
@@ -5,72 +5,14 @@
  */
 
 const http = require('http');
-const url = require('url');
-const StringDecoder = require('string_decoder').StringDecoder;
-const router = require('./router');
-const config = require('./config');
+const config = require('./src/config');
+const unifiedServer = require('./src/server');
 
 // create the server
 const server = http.createServer((req, res) => {
 
-  // getting the path
-  const parsedUrl = url.parse(req.url, true);
-  const path = parsedUrl.pathname;
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  // http method
-  const method = req.method.toLowerCase();
-
-  // payload
-  // binding to listen the 'data' event to receive the payload
-  const decoder = new StringDecoder('utf-8');
-  let buffer = '';
-  req.on('data', (data) => {
-    buffer += decoder.write(data);
-  });
-
-  // request object emits an 'end' signal (handler of the 'end' event)
-  req.on('end', () => {
-    buffer += decoder.end();
-
-    // construct the data object to send to the handler
-    const data = {
-      'trimmedPath' : trimmedPath,
-      'method' : method,
-      'payload' : buffer,
-    };
-
-    // choose the handler this request should go to
-    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
-      ? router[trimmedPath]
-      : router.notFound;
-
-    // route the request to the handler specified in the router
-    chosenHandler(data, (statusCode, payload) => {
-      // use the status code called back by the handler
-      statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
-
-      // use the payload called back by the handler
-      payload = typeof(payload) === 'object' ? payload : {};
-
-      // Convert the payload to String
-      const payloadString = JSON.stringify(payload);
-
-      // Return the response in JSON format
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(payloadString);
-
-      //
-      console.log('sending back to user: ', payloadString);
-    });
-
-
-    console.log(trimmedPath);
-    console.log(method);
-    console.log(buffer);
-
-  });
+  // create the server
+  unifiedServer(req, res);
 
 });
 
---
+++ src/server/index.js
@@ -0,0 +1,71 @@
+/*
+ * Unified instructions to the server
+ *
+ */
+
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
+const router = require('../router');
+
+const unifiedServer = (req, res) => {
+  // getting the path
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // http method
+  const method = req.method.toLowerCase();
+
+  // payload
+  // binding to listen the 'data' event to receive the payload
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', (data) => {
+    buffer += decoder.write(data);
+  });
+
+  // request object emits an 'end' signal (handler of the 'end' event)
+  req.on('end', () => {
+    buffer += decoder.end();
+
+    // construct the data object to send to the handler
+    const data = {
+      'trimmedPath' : trimmedPath,
+      'method' : method,
+      'payload' : buffer,
+    };
+
+    // choose the handler this request should go to
+    const chosenHandler = typeof(router[trimmedPath]) !== 'undefined'
+      ? router[trimmedPath]
+      : router.notFound;
+
+    // route the request to the handler specified in the router
+    chosenHandler(data, (statusCode, payload) => {
+      // use the status code called back by the handler
+      statusCode = typeof(statusCode) === 'number' ? statusCode : 200;
+
+      // use the payload called back by the handler
+      payload = typeof(payload) === 'object' ? payload : {};
+
+      // Convert the payload to String
+      const payloadString = JSON.stringify(payload);
+
+      // Return the response in JSON format
+      res.setHeader('Content-Type', 'application/json');
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      //
+      console.log('sending back to user: ', payloadString);
+    });
+
+
+    console.log(trimmedPath);
+    console.log(method);
+    console.log(buffer);
+
+  });
+};
+
+module.exports = unifiedServer;
